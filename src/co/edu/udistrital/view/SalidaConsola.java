4659fa9285e7278cf0e07c49af5df285e4ca3568
NikoOtitap <nicotitap@mail>
2026-08-12 16:52:27 -0500

    fix: minor output fix

--- src/co/edu/udistrital/view/SalidaConsola.java
+++ src/co/edu/udistrital/view/SalidaConsola.java
@@ -3,6 +3,6 @@ package co.edu.udistrital.view;
 public class SalidaConsola implements Salida{
     @Override
     public void mostrar(String texto) {
-        System.out.println("el resultado es " + resultado);
+        System.out.println(texto);
     }
 }
