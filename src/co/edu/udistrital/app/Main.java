a3e477e4c2394d51c83bdebdb66e3b12e054efd0
NikoOtitap <nicotitap@mail>
2026-08-21 17:06:22 -0500

    refactor: organized the code better

--- src/co/edu/udistrital/app/Main.java
+++ src/co/edu/udistrital/app/Main.java
@@ -1,7 +1,6 @@
 package co.edu.udistrital.app;
 
 
-import co.edu.udistrital.cliente.Cliente;
 import co.edu.udistrital.factory.FabricaArchivo;
 import co.edu.udistrital.factory.FabricaConsola;
 import co.edu.udistrital.factory.FabricaEntradaSalida;
@@ -13,11 +12,12 @@ import static java.lang.Double.parseDouble;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        FabricaEntradaSalida fabrica = new FabricaArchivo();
+        FabricaEntradaSalida fabrica = new FabricaConsola();
         Operacion operacion = new Suma();
         Entrada entrada = fabrica.crearEntrada();
         Salida salida = fabrica.crearSalida();
-
+        
+        salida.mostrar("ingrese dos numeros");
         double a = parseDouble(entrada.capturar());
         double b = parseDouble(entrada.capturar());
 
